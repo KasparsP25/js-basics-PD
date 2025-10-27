e648750447afec92221db5fa033e54da614baa41
DeivisStasuls <deivis.stasuls@gmail.com>
2025-10-27 10:51:39 +0200

    added ticket price boolean

---
+++ ticket-price.js
@@ -0,0 +1,12 @@
+// - Bērniem līdz 6 gadu vecumam (ieskaitot) ieeja ir bez maksas.
+//    - Skolēniem un studentiem (`isStudent` ir patiess) biļetes cena ir 5 EUR.
+//    - Visiem pārējiem biļetes cena ir 10 EUR.
+let visitorAge = 12;
+let isStudent = true;
+if (visitorAge < 6){
+    console.log("ieeja ir bez maksas");
+} else if (isStudent = true) {
+    console.log("biļetes cena ir 5 EUR");
+} else {
+    console.log("biļetes cena ir 10 EUR.");
+}
